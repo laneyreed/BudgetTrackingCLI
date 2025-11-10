2a6bf4d3d11898766d406cd3b32bacc6b2c58ec4
Shanreed25 <tralainereed@gmail.com>
2025-11-10 10:58:19 -0800

    adding generic login and register

--- CLI/main.py
+++ CLI/main.py
@@ -1,13 +1,3 @@
-# import sys
-# from pathlib import Path
-
-# # Add the project directory to Python path
-# project_dir = Path(__file__).parent
-# sys.path.insert(0, str(project_dir))
-
-# Now import and run the CLI with absolute imports
-
-
 from ui.login import main
 
 if __name__ == "__main__":
--- CLI/ui/login.py
+++ CLI/ui/login.py
@@ -1,2 +1,54 @@
+from rich.console import Console
+from rich.text import Text
+from rich import box
+from rich.panel import Panel
+
+def printHeader():
+    console = Console()
+    # header_text = Text("💰 BUDGET TRACKER 💰", justify="center")
+    # console.print(Panel(header_text, box=box.DOUBLE))
+    console.print("\n[header]Welcome to the Application CLI![/header]\n", justify="center")
+
+#================================================================
+# Are you a new user or existing user?
+# new user = login
+# existing user = register
+#================================================================
+
+#================================================================
+# Generic login function
+#================================================================
+def login():
+    # for now create a generic login flow
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
+    print(f"Logging in as: {username}")
+
+
+
+#================================================================
+# Generic register function
+#================================================================
+def register():    # for now create a generic register flow
+    username = input("Choose a username: ")
+    password = input("Choose a password: ")
+    print(f"Registering new user: {username}.....")
+    login()
+
+def loginOrRegister():
+    while True:
+        choice = input("Enter 1 to login or 2 to register: ")
+        if choice == "1":
+            login()
+            break
+        elif choice == "2":
+            register()
+            break
+        else:
+            print("Invalid choice. Please enter 1 or 2.")
+
 def main():
-    print("This is The Budget Tracker Please Log In.")
+    while True:
+        printHeader()
+        loginOrRegister()
+        break
